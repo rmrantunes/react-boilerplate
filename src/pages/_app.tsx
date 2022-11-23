e33a793377f21572adab04127b014c280c031176
Rafael Antunes <rafaelmessias710@gmail.com>
2022-11-22 20:45:09 -0400

    refactor: _app.tsx with new content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { GlobalStyles } from 'styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="sortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="Description"
+          content="A simple project starter with Typescript, React, Next JS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
