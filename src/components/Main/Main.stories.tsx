f3ded88aa84e716790eb40fd181d85a33c6625b3
Rafael Antunes <rafaelmessias710@gmail.com>
2022-11-24 08:33:20 -0400

    feat: add initial controls to Main story

--- src/components/Main/Main.stories.tsx
+++ src/components/Main/Main.stories.tsx
@@ -1,9 +1,17 @@
-import { Meta } from '@storybook/react/types-7-0'
-import { Main } from '.'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import { Main, MainProps } from '.'
 
 export default {
   title: 'Main',
-  component: Main
-} as Meta
+  component: Main,
+  args: {
+    description: 'default description',
+    title: 'default title'
+  }
+} as Meta<MainProps>
 
-export const Default = () => <Main />
+export const Default: Story<MainProps> = (args) => <Main {...args} />
+
+Default.args = {
+  title: 'React avançado!'
+}
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,21 @@
 import * as S from './styles'
 
-export function Main() {
+export type MainProps = {
+  title?: string
+  description?: string
+}
+
+export function Main(props: MainProps) {
   return (
     <S.Wrapper>
       <S.Logo
         src="/img/logo.svg"
         alt="Imagem de um átomo e 'React Avançado' escrito ao lado"
       />
-      <S.Title>React avançado</S.Title>
-      <S.Description>ReactJs, NextJs e Styled Components</S.Description>
+      <S.Title>{props.title || 'React avançado'}</S.Title>
+      <S.Description>
+        {props.description || 'ReactJs, NextJs e Styled Components'}
+      </S.Description>
       <S.Illustration
         src="/img/hero-illustration.svg"
         alt="Desenvolvedor de frente pra tela com código"
