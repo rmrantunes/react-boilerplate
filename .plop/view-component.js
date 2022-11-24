eae5fb09752a964a967adba8ef216b251a4ac055
Rafael Antunes <rafaelmessias710@gmail.com>
2022-11-24 19:34:16 -0400

    feat: add more generator types
    lib, app, view

---
+++ .plop/view-component.js
@@ -0,0 +1,39 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'application component',
+
+    // inquirer prompts
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name?'
+      }
+    ],
+
+    // actions to perform
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/views/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/component.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/views/{{pascalCase name}}/styles.tsx',
+        templateFile: 'templates/styles.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/views/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/stories.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/views/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/component-test.hbs'
+      }
+    ]
+  })
+};
+  
